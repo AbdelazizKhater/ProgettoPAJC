d86410fab672af055adec8746533efaa6aff9197
AbdelazizKhater <abdelazizyoussef2002@gmail.com>
2024-12-18 18:57:20 +0100

    maybe fix

--- src/it/unibs/pajc/GameView.java
+++ src/it/unibs/pajc/GameView.java
@@ -22,17 +22,26 @@ public class GameView extends JPanel {
         this.add(infoPanel, BorderLayout.NORTH);
         this.add(gameFieldPanel, BorderLayout.CENTER);
 
-        // Animation Timer
-        Timer timer = new Timer(5, e -> {
+        Timer timer = new Timer(0, e -> {
+            long startTime = System.nanoTime();
+
             cntrl.stepNext();
-
-
             infoPanel.update();
-            
             repaint();
+
+            long elapsedTime = (System.nanoTime() - startTime) / 1_000_000; // Tempo in ms
+
+            if(elapsedTime > 4)
+            System.out.println(elapsedTime);
+
+            // Calcola il tempo rimanente per il prossimo frame
+            int delay = Math.max(1, 8 - (int) elapsedTime);
+
+            ((Timer) e.getSource()).setDelay(delay);
         });
 
         timer.start();
+
     }
 
 }
